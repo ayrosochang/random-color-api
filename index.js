2a51d51e1a5733ae46910d2bf7e49bc411b8f8ff
Gelo Chang <ayrosochang16@gmail.com>
2020-12-16 17:19:21 +0800

    update port

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const server = new ApolloServer({ typeDefs, resolvers });
  
 server.applyMiddleware({ app })
  
-// app.listen({ port: 4000 }, () =>
-app.listen(() =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
